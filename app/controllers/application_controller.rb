9b44a64e6bec4dee62a63d6f2dd5ce0583b2e2f4
keza681 <lindakeza19@gmail.com>
2022-07-15 10:57:57 +0200

    Add relations in the models

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+    belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+    belongs_to :posts
+    after_save :update_comments_counter
+  
+    def update_comments_counter
+      post.increment!(:comments_counter)
+    end
 end
