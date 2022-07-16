558a00b6ffcd429ec638362e9f9992874730e359
keza681 <lindakeza19@gmail.com>
2022-07-16 20:56:34 +0200

    Correct linters errors using rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
-    has_many :post
-    has_many :comment
-    has_many :like
-  
-    validates :name, presence: true
-    validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  
-    def three_recent_posts
-      post.includes(:user).order(created_at: :desc).limit(3)
-    end
+  has_many :post
+  has_many :comment
+  has_many :like
+
+  validates :name, presence: true
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  def three_recent_posts
+    post.includes(:user).order(created_at: :desc).limit(3)
   end
+end
