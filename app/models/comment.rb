142d421477c12ba1c49e8dcdc2e6db3fd5a784f7
keza681 <lindakeza19@gmail.com>
2022-07-16 20:54:41 +0200

    Implement changes required by reviewer

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,8 @@
 class Comment < ApplicationRecord
-  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-  belongs_to :posts
-  after_save :update_comments_counter
+  belongs_to :user
+  belongs_to :post
 
   def update_comments_counter
-    post.increment!(:comments_counter)
+    post.update(comments_counter: post.comment.count)
   end
 end
