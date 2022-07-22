3e679144d5c50b0cb684f08fba146d26507f7e4f
keza681 <lindakeza19@gmail.com>
2022-07-22 21:46:30 +0200

    Correct linters errors using rubocop

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,32 +2,34 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
-    before(:example) { get '/users' }
+    before(:example) { get users_path }
+
     it 'should respond with code 200' do
       expect(response).to have_http_status(:ok)
     end
 
-    it 'should render users index view' do
+    it 'should render users list view' do
       expect(response).to render_template(:index)
     end
 
     it 'should show correct content in the view' do
-      expect(response.body).to include('list of all usres')
+      expect(response.body).to include('Here is a list of all users')
     end
   end
 
   describe 'GET /show' do
-    before(:example) { get '/users/1' }
+    before(:example) { get user_path(1) }
+
     it 'should respond with code 200' do
       expect(response).to have_http_status(:ok)
     end
 
-    it 'should render users show view' do
+    it 'should render user detail view' do
       expect(response).to render_template(:show)
     end
 
     it 'should show correct content in the view' do
-      expect(response.body).to include('User Details')
+      expect(response.body).to include('Here is the details of the given user based on his user_id')
     end
   end
 end
