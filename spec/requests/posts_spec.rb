11c95c5bafe354570b5598bc0be461b38ca9eb1d
keza681 <lindakeza19@gmail.com>
2022-07-22 21:23:01 +0200

    Add post controller test to the project

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,16 +2,34 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
-    it 'returns http success' do
-      get '/posts/index'
-      expect(response).to have_http_status(:success)
+    before(:example) { get user_posts_path(1) }
+
+    it 'should respond with code 200' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'should render posts list view' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'should show correct content in the view' do
+      expect(response.body).to include('Here is a list of all posts for a given user based on the given user_id')
     end
   end
 
-  describe 'GET /show' do
-    it 'returns http success' do
-      get '/posts/show'
-      expect(response).to have_http_status(:success)
+  describe 'GET /index' do
+    before(:example) { get user_post_path(1, 1) }
+
+    it 'should respond with code 200' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'should render posts list view' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'should show correct content in the view' do
+      expect(response.body).to include('Here is the details of the given post for the given user')
     end
   end
 end
