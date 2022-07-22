e7a88eb74d489add3c60a4cb2c493db8a5d42a06
keza681 <lindakeza19@gmail.com>
2022-07-22 20:15:31 +0200

    Generate posts controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+  get 'posts/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
