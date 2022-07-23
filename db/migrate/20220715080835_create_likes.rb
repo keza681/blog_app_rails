3e2cf191633f78e28dc72aa1ac4e562720a35168
keza681 <lindakeza19@gmail.com>
2022-07-23 13:48:15 +0200

    Generate migrations

--- db/migrate/20220715080835_create_likes.rb
+++ db/migrate/20220715080835_create_likes.rb
@@ -1,9 +1,8 @@
 class CreateLikes < ActiveRecord::Migration[7.0]
   def change
     create_table :likes do |t|
-      t.references :author, foreign_key: {to_table: 'users'}
-
-      t.belongs_to :posts, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+      t.references :post, null: false, foreign_key: true
 
       t.timestamps
     end
