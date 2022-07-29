7b5a6bffbcf9c9762f001e104b26f381d5366347
keza681 <lindakeza19@gmail.com>
2022-07-29 22:54:26 +0200

    Implement permission parameter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :null_session
+  respond_to :json
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  before_action :authenticate_user
+  skip_before_action :verify_authenticity_token
   protected
 
   def configure_permitted_parameters
