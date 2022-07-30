7cea4a7473960e9713a9307f0b8ce8a0ffcb838c
Alfredbis29 <Kalumunabisimwa5@gmail.com>
2022-07-30 08:34:10 +0200

    fix the controller error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,9 @@
 class UsersController < ApplicationController
   def index
-    redirect_to new_user_session_path if current_user.nil?
     @users = User.all
   end
 
   def show
     @user = User.find(params[:id])
-    @recent_posts = @user.three_recent_posts
   end
 end
